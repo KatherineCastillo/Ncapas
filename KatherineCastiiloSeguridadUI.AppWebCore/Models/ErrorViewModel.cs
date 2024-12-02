fc3faa0e12bb4e1e75d1c615710d749b2b5342b2
KatherineCastillo <castillokathy2004@gmail.com>
2024-12-02 17:20:14 -0600

    primer cambio

---
+++ KatherineCastiiloSeguridadUI.AppWebCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KatherineCastiiloSeguridadUI.AppWebCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
